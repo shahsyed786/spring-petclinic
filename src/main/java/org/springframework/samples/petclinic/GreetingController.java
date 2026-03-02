4cc535c1b9d77084ded3492ab7a812da51fa8a28
shahsyed786 <ksyed11@my.centennialcollege.ca>
2026-03-01 22:24:54 -0500

    Update GreetingController.java
    Signed-off-by: shahsyed786 <ksyed11@my.centennialcollege.ca>

--- src/main/java/org/springframework/samples/petclinic/GreetingController.java
+++ src/main/java/org/springframework/samples/petclinic/GreetingController.java
@@ -1,26 +1,23 @@
 package org.springframework.samples.petclinic;
 
-import java.time.LocalTime;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class GreetingController {
+public class GreetingController {
 
-	@GetMapping("/comp367")
-	String greeting() {
-		int hour = LocalTime.now().getHour();
-		String message;
+    @GetMapping("/comp367")
+    public String greeting() {
 
-		if (hour < 12) {
-			message = "Good morning, Shah Syed, Welcome to COMP367";
-		}
-		else {
-			message = "Good afternoon, Shah Syed, Welcome to COMP367";
-		}
+        int hour = 15;   // forces afternoon
+        String message;
 
-		return message;
-	}
+        if (hour < 12) {
+            message = "Good morning, Shah Syed, Welcome to COMP367";
+        } else {
+            message = "Good afternoon, Shah Syed, Welcome to COMP367";
+        }
 
+        return message;
+    }
 }
